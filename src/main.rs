b1d951eed9b0565379c987abd65e13bfb29418d7
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-12-29 00:58:46 +0100

    better error handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod error;
 mod plugin;
 
 use nu_plugin::{serve_plugin, MsgPackSerializer};
--- src/plugin/from_kdl.rs
+++ src/plugin/from_kdl.rs
@@ -1,8 +1,38 @@
 use super::FromAdvPlugin;
 use kdl::{KdlDocument, KdlEntry, KdlNode, KdlValue};
+use miette::NarratableReportHandler;
 use nu_plugin::EvaluatedCall;
 use nu_plugin::{EngineInterface, PluginCommand, SimplePluginCommand};
 use nu_protocol::{LabeledError, Record, Signature, Span, Type, Value as NuValue};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Unable to parse KDL document: {}", Self::from_kdl_error(.0))]
+    KdlParseError(#[from] kdl::KdlError),
+
+    #[error("Unable to convert from {from} to {to} (value: {value:?})")]
+    BadConversion {
+        from: String,
+        to: String,
+        value: Option<String>,
+    },
+}
+
+impl Error {
+    fn from_kdl_error(error: &kdl::KdlError) -> String {
+        let report_handler = NarratableReportHandler::new();
+
+        let mut rendered = error
+            .diagnostics
+            .iter()
+            .fold(String::new(), |mut acc, diag| {
+                let _ = report_handler.render_report(&mut acc, diag);
+                acc
+            });
+        rendered.pop();
+        rendered
+    }
+}
 
 pub struct FromKdl;
 
@@ -36,7 +66,7 @@ impl SimplePluginCommand for FromKdl {
         match input {
             NuValue::String { val, .. } => Self::parse(&val).map_err(|e| {
                 LabeledError::new("Unable to convert kdl input to nu value")
-                    .with_label(e, call.head)
+                    .with_label(e.to_string(), call.head)
             }),
             _ => Err(
                 LabeledError::new("Expected String input from pipeline").with_label(
@@ -48,19 +78,22 @@ impl SimplePluginCommand for FromKdl {
     }
 }
 
+type ResultValue = Result<NuValue, Error>;
+
 impl FromKdl {
-    fn parse(input: &str) -> Result<NuValue, String> {
-        let kdoc: KdlDocument = input
-            .parse()
-            .map_err(|e| format!("Unable to parse kdl from input string: {e:#?}"))?;
+    fn parse(input: &str) -> ResultValue {
+        let kdoc: KdlDocument = input.parse()?;
         Self::convert_document(&kdoc)
     }
-    fn convert_value(kvalue: &KdlValue) -> Result<NuValue, String> {
+    fn convert_value(kvalue: &KdlValue) -> ResultValue {
         Ok(match kvalue {
             KdlValue::String(s) => NuValue::string(s, Span::unknown()),
             KdlValue::Integer(i) => NuValue::int(
-                (*i).try_into()
-                    .map_err(|_| "out of bound integer".to_string())?,
+                (*i).try_into().map_err(|_| Error::BadConversion {
+                    from: "i128".into(),
+                    to: "i64".into(),
+                    value: Some(i.to_string()),
+                })?,
                 Span::unknown(),
             ),
             KdlValue::Float(v) => NuValue::float(*v, Span::unknown()),
@@ -68,10 +101,10 @@ impl FromKdl {
             KdlValue::Null => NuValue::nothing(Span::unknown()),
         })
     }
-    fn convert_entries(kentries: &[KdlEntry]) -> Result<NuValue, String> {
+    fn convert_entries(kentries: &[KdlEntry]) -> ResultValue {
         let nu_entries = kentries
             .iter()
-            .map(|kentry| -> Result<NuValue, String> {
+            .map(|kentry| -> ResultValue {
                 let mut rec = Record::new();
                 rec.push(
                     "name",
@@ -83,18 +116,18 @@ impl FromKdl {
                 rec.push("value", Self::convert_value(kentry.value())?);
                 Ok(NuValue::record(rec, Span::unknown()))
             })
-            .collect::<Result<Vec<NuValue>, String>>()?;
+            .collect::<Result<Vec<NuValue>, Error>>()?;
         Ok(NuValue::list(nu_entries, Span::unknown()))
     }
-    fn convert_document(kdoc: &KdlDocument) -> Result<NuValue, String> {
+    fn convert_document(kdoc: &KdlDocument) -> ResultValue {
         let nu_nodes = kdoc
             .nodes()
             .iter()
             .map(Self::convert_node)
-            .collect::<Result<Vec<NuValue>, String>>()?;
+            .collect::<Result<Vec<NuValue>, Error>>()?;
         Ok(NuValue::list(nu_nodes, Span::unknown()))
     }
-    fn convert_node(knode: &KdlNode) -> Result<NuValue, String> {
+    fn convert_node(knode: &KdlNode) -> ResultValue {
         let mut rec = Record::new();
         rec.push(
             "name",
